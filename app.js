d505e2e3d7904a68d8976a406a36b3ca511e1426
nodeDevcoder <jibrilasif@gmail.com>
2022-04-03 15:24:48 -0700

    add server listening

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config();
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config();
+// }
 
 const express = require('express'),
     session = require('express-session'),
     app = express();
 
+app.use(session({
+    name: 'session',
+    secret: 'jibril',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        name: 'user-cookie',
+        maxAge: 1000 * 60 * 5,
+        httpOnly: true,
+        signed: true
+    }
+}));
+
+let port = 3000;
+app.listen(3000, () => {
+    console.log("Server is listening on port ", port);
+});
